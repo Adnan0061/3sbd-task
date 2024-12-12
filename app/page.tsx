aec6fd1b4a786b53c56f306c9333ac23932288fd
Adnan Ahmed <adnan.ahmed0061@gmail.com>
2024-12-12 12:41:28 +0600

    .

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function CompaniesPage() {
-  return (
-    <div className="h-full w-full p-6 space-y-6">
-      <h3>Page not found. go to companies list page</h3>
-    </div>
-  );
-}
